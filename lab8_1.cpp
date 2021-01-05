f404e16beb7a59e0ec35f65d1efbee6d2cc540e3
swik999 <76035329+swik999@users.noreply.github.com>
2021-01-05 16:22:41 +0700

    Update lab8_1.cpp

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 int main(){	
 	cout << "Enter initial loan: ";
+	double MONEY = 0;
+	cin >> MONEY;
 	cout << "Enter interest rate per year (%): ";
+	double DOK = 0;
+	cin >> DOK;
 	cout << "Enter amount you can pay per year: ";
+	double PAY = 0;
+	cin >> PAY;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -18,16 +24,33 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	int year = 1;
+	double PrevBalance = MONEY;
+	
+	while (PrevBalance > 0){
+		
+		double Interest = (PrevBalance*(DOK/100));
+	    double Total = (PrevBalance + Interest);
+	    cout << fixed << setprecision(2); 
+	    cout << setw(13) << left << year ; 
+	    cout << setw(13) << left << PrevBalance;
+	    cout << setw(13) << left << Interest;
+	    cout << setw(13) << left << Total;
+	    if (Total < PAY){
+	    	cout << setw(13) << left << Total;
+	    }else{
+	    	cout << setw(13) << left << PAY;
+	    }
+	    double NewBalance = (Total-PAY) ;
+	    if(NewBalance < 0){
+	    	cout << setw(13) << left << 0.00;
+		}else{
+		cout << setw(13) << left << NewBalance;	
+		}
+	    year++;
+	    PrevBalance = NewBalance ;
+	    cout << "\n";	
+	}
 	
 	return 0;
 }
